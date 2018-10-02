91edf134216da3f5bedfe706eaf80a3de08b694c
Pavel Gre4ixin <undeadhd@yandex.ru>
2018-10-03 00:17:06 +0300

    add xcode template

---
+++ Reactor Template.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,19 @@
+//___FILEHEADER___
+
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class ___FILEBASENAME___: UIViewController, StoryboardView {
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func bind(reactor: ViewController.Reactor) {
+        //binding here
+    }
+    
+}
